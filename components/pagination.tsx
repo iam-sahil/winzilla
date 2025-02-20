cf8baf0def2e080a68fbf43c5aaaa0c04d4a93c8
iam-sahil <sss.lata82@gmail.com>
2025-02-21 00:12:44 +0530

    .

--- components/pagination.tsx
+++ components/pagination.tsx
@@ -1,8 +1,4 @@
-import { getPreviousNext } from "@/lib/markdown";
-
 export default function Pagination({ pathname }: { pathname: string }) {
-  const res = getPreviousNext(pathname);
-
   return (
     <div className="sm:py-10 py-7">
       <hr />
